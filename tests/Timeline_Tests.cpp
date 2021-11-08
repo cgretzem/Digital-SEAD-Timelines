80d37ae8207bd8b8297911f94ad46a34a57dfb40
dsmith265 <72399454+dsmith265@users.noreply.github.com>
2021-11-08 09:35:01 -0700

    Update Timeline_Tests.cpp

--- tests/Timeline_Tests.cpp
+++ tests/Timeline_Tests.cpp
@@ -4,11 +4,12 @@ int main()
 {
    
     cout<<"Standard example 1"<<endl;	
+                  //( Time on Target, Supression Time of flight, Mark Type, Mark Time of flight, Timeline type )
     Standard_Timeline t0 = Standard_Timeline(15, 25, ILLUM, 30, CONTINUOUS);
     t0.print();
 
     cout<<"Standard example 2"<<endl;
-
+                                      // ( TOT, STOF, Mark, Mark TOF, Type )
     Standard_Timeline t1 = Standard_Timeline(20, 31, WP, 40, INTERRUPTED);
     t1.print();
 
@@ -17,7 +18,7 @@ int main()
     frVec.push_back(fireRange{-2, 1}); // 1 - (-2) = 3 min interval
     //cout<<"gap"<<endl;               // 3 - 1 = 2 min interval
     frVec.push_back(fireRange{3, 5}); // 5 - 3 = 2 min interval
-
+                                    //( CAS TOT, Supression TOF, fire range, fire rate )
     NonStandard_Timeline nST = NonStandard_Timeline(40, 30, frVec, 30);
     nST.print();
 
