5e2a32f31f6bfb8a90773a5663670a311f0aa1a1
dsmith265 <72399454+dsmith265@users.noreply.github.com>
2021-11-10 10:33:22 -0700

    Add files via upload
    added marking round for NonStandard
    added Round numbers and counts
    changed a few comments

---
+++ timeline_Revision.cpp
@@ -0,0 +1,299 @@
+#include "timeline.h"
+#include <algorithm>
+using namespace std;
+
+bool shell::operator==(shell rhs)
+{
+    if (rhs.fireTime == this->fireTime && rhs.isHE == this->isHE)
+        return true;
+    return false;
+}
+bool shell::operator<=(shell rhs)
+{
+    if (*this == rhs)
+        return true;
+    return fireTime <= rhs.fireTime;
+}
+bool shell::operator>=(shell rhs)
+{
+    if (*this == rhs)
+        return true;
+    return fireTime >= rhs.fireTime;
+}
+bool shell::operator<(shell rhs)
+{
+    if (*this == rhs)
+        return true;
+    return fireTime < rhs.fireTime;
+}
+bool shell::operator>(shell rhs)
+{
+    if (*this == rhs)
+        return true;
+    return fireTime > rhs.fireTime;
+}
+
+
+
+Timeline::Timeline(int TOT, int TOF)
+{
+    this->timeOnTarget = TOT;
+    this->timeOfFlight = TOF;
+    this->startTime = 0;
+    //Timeline is abstract, timeList needs to be initalized in derived classes
+}
+Timeline::Timeline(int startTime, int TOT, int TOF)
+{
+    this->timeOnTarget = TOT;
+    this->timeOfFlight = TOF;
+    this->startTime = startTime;
+
+    //Timeline is abstract, timeList needs to be initalized in derived classes
+}
+
+int Timeline::getTOT()
+{
+    return this->timeOnTarget;
+}
+
+int Timeline::getTOF()
+{
+    return this->timeOfFlight;
+}
+
+
+std::vector<shell> Timeline::getTimeList()
+{
+    return this->timeList;
+}
+
+void Timeline::sortTimeList()
+{
+    if (timeList.empty())
+        return;
+    sort(timeList.begin(), timeList.end());
+}
+
+void Timeline::print()
+{
+    for (shell s : timeList)
+    {
+        cout << s.fireTime << " : (HE)" << endl;
+    }
+}
+
+Standard_Timeline::Standard_Timeline(int TOT, int TOF, markingType mType, int MTOF, timelineType tType) : Timeline(TOT, TOF)
+{
+    this->mType = mType;
+    this->markingTOF = MTOF;
+    this->tType = tType;
+    this->startTime = startTime;
+    this->timeList = this->makeTimelist();
+}
+
+std::vector<shell> Standard_Timeline::makeTimelist()
+{
+    if (tType == CONTINUOUS)
+    {
+        //takes time on target in minutes, subtracts (TOF+5), and a minute because HE 1 hits 1 min before TOT
+        Sexagesimal firstHE = Sexagesimal(startTime, timeOnTarget, 0) - Sexagesimal(0, 1, timeOfFlight + 5);
+        timeList.push_back(shell{ firstHE, true });
+        //subtracts 30 seconds because HE 2 hits 30 seconds before TOT
+        Sexagesimal secondHE = Sexagesimal(startTime, timeOnTarget, 0) - Sexagesimal(0, 0, timeOfFlight + 5 + 30);
+        timeList.push_back(shell{ secondHE, true });
+
+        //puts a marking round into the list if one is specified, -45 for illum and -30 for wp. Time of flight for
+        //supression is also different, so must use STOF instead of regular TOF.
+        Sexagesimal marking;
+        switch (mType)
+        {
+        case ILLUM:
+            marking = Sexagesimal(startTime, timeOnTarget, 0) - Sexagesimal(0, 0, markingTOF + 5 + 45);
+            timeList.push_back(shell{ marking, false });
+            break;
+        case WP:
+            //marking not TOF?
+            marking = Sexagesimal(startTime, timeOnTarget, 0) - Sexagesimal(0, 0, markingTOF + 5 + 30);
+            timeList.push_back(shell{ marking, false });
+        case NEGATIVE:
+            break;
+        }
+        //Round is supposed to hit at TOT specified, so just subtract Time of Flight
+        Sexagesimal thirdHE = Sexagesimal(startTime, timeOnTarget, 0) - Sexagesimal(0, 0, timeOfFlight + 5);
+        timeList.push_back(shell{ thirdHE, true });
+
+        //HE round 4 hits at +30, so just subtract time of flight from TOT and then add 30
+        Sexagesimal fourthHE = (Sexagesimal(startTime, timeOnTarget, 0) - Sexagesimal(0, 0, timeOfFlight + 5)) + Sexagesimal(0, 0, 30);
+        timeList.push_back(shell{ fourthHE, true });
+
+        //HE round 4 hits at +1 min, so just subtract time of flight from TOT and then add 1 min
+        Sexagesimal fifthHE = (Sexagesimal(startTime, timeOnTarget, 0) - Sexagesimal(0, 0, timeOfFlight + 5)) + Sexagesimal(0, 1, 0);
+        timeList.push_back(shell{ fifthHE, true });
+    }
+    else// INTERRUPTED timelineType
+    {
+        //takes time on target in minutes, subtracts (TOF+5), and a minute because HE 1 hits 1 min before TOT
+        Sexagesimal firstHE = Sexagesimal(startTime, timeOnTarget, 0) - Sexagesimal(0, 1, timeOfFlight + 5);
+        timeList.push_back(shell{ firstHE, true });
+        //subtracts 30 seconds because HE 2 hits 30 seconds before TOT
+        Sexagesimal secondHE = Sexagesimal(startTime, timeOnTarget, 0) - Sexagesimal(0, 0, timeOfFlight + 5 + 30);
+        timeList.push_back(shell{ secondHE, true });
+
+        //puts a marking round into the list if one is specified, -45 for illum and -30 for wp. Time of flight for
+        //supression is also different, so must use STOF instead of regular TOF.
+        Sexagesimal marking;
+        switch (mType)
+        {
+        case ILLUM:
+            marking = Sexagesimal(startTime, timeOnTarget, 0) - Sexagesimal(0, 0, markingTOF + 5 + 45);
+            timeList.push_back(shell{ marking, false });
+            break;
+        case WP:
+            marking = Sexagesimal(startTime, timeOnTarget, 0) - Sexagesimal(0, 0, markingTOF + 5 + 30);
+            timeList.push_back(shell{ marking, false });
+            break;
+        case NEGATIVE:
+            break;
+        }
+    }
+
+    //sorting the timeList by fireTime
+    sortTimeList();
+    return timeList;
+
+}
+
+void Standard_Timeline::print()
+{
+    int countHE = 0;
+    int countIllum = 0;
+    int countWP = 0;
+    int countRounds = 1;
+   
+    for (shell s : timeList)
+    {
+        if (s.isHE) {
+            cout << "Round " << countRounds << ": " << s.fireTime << " : (HE)" << endl;
+            countHE++;
+        }
+        else
+        {
+            if (mType == ILLUM) {
+                cout << "Round " << countRounds << ": " << s.fireTime << " : (ILLUM)" << endl;
+                countIllum++;
+            }
+            else {
+                cout << "Round " << countRounds << ": " << s.fireTime << " : (WP)" << endl;
+                countWP++;
+            }
+        }
+    }
+    
+    cout << "\n HE count: " << countHE << endl;
+    cout << "\n WP count: " << countWP << endl;
+    cout << "\n ILLUM count: " << countIllum << endl;
+    cout << "\n";
+}
+
+int Standard_Timeline::getMTOF()
+{
+    return this->markingTOF;
+}
+
+markingType Standard_Timeline::getMarkingType()
+{
+    return this->mType;
+}
+
+timelineType Standard_Timeline::getTimelineType()
+{
+    return this->tType;
+}
+
+
+NonStandard_Timeline::NonStandard_Timeline(int TOT, int TOF, markingType mType, int MTOF, std::vector<fireRange> firingRanges, int fireFreq) : Timeline(TOT, TOF)
+{
+    this->mType = mType;
+    this->markingTOF = MTOF;
+    this->startTime = startTime;
+    this->fireFreq = fireFreq;
+    this->firingRanges = firingRanges;
+    this->timeList = this->makeTimelist();
+}
+
+std::vector<shell> NonStandard_Timeline::makeTimelist()
+{
+    Sexagesimal currentTime;
+    for (fireRange fr : firingRanges)
+    {
+        currentTime = Sexagesimal(startTime, timeOnTarget, 0) + Sexagesimal(0, fr.startMin, 0) - Sexagesimal(0, 0, timeOfFlight + 5);
+        while (currentTime <= (Sexagesimal(startTime, timeOnTarget, 0) + Sexagesimal(0, fr.endMin, 0) - Sexagesimal(0, 0, timeOfFlight + 5)))
+        {
+            timeList.push_back(shell{ currentTime, true });
+            currentTime = currentTime + Sexagesimal(0, 0, fireFreq);
+        }
+    }
+
+    //puts a marking round into the list if one is specified, -45 for illum and -30 for wp. Time of flight for
+    //supression is also different, so must use STOF instead of regular TOF.
+    Sexagesimal marking;
+    switch (mType)
+    {
+    case ILLUM:
+        marking = Sexagesimal(startTime, timeOnTarget, 0) - Sexagesimal(0, 0, markingTOF + 5 + 45);
+        timeList.push_back(shell{ marking, false });
+        break;
+    case WP:
+        //marking not TOF?
+        marking = Sexagesimal(startTime, timeOnTarget, 0) - Sexagesimal(0, 0, markingTOF + 5 + 30);
+        timeList.push_back(shell{ marking, false });
+    case NEGATIVE:
+        break;
+    }
+
+    sortTimeList();
+    return timeList;
+}
+
+void NonStandard_Timeline::print()
+{
+    int countHE = 0;
+    int countIllum = 0;
+    int countWP = 0;
+    int countRounds = 1;
+    for (shell s : timeList)
+    {
+        if (s.isHE) {
+            cout << "Round "<<countRounds<<": " << s.fireTime << " : (HE)" << endl;
+            countHE++;
+            countRounds++;
+        }
+        else
+        {
+            if (mType == ILLUM) {
+                cout << "Round " << countRounds << ": " << s.fireTime << " : (ILLUM)" << endl;
+                countIllum++;
+                countRounds++;
+            }
+            else {
+                cout << "Round " << countRounds << ": " << s.fireTime << " : (WP)" << endl;
+                countWP++;
+                countRounds++;
+            }
+        }
+    }
+    cout << "\n HE count: " << countHE << endl;
+    cout << "\n WP count: " << countWP << endl;
+    cout << "\n ILLUM count: " << countIllum << endl;
+    cout << "\n";
+}
+
+int NonStandard_Timeline::getMTOF()
+{
+    return this->markingTOF;
+}
+
+markingType NonStandard_Timeline::getMarkingType()
+{
+    return this->mType;
+}
---
+++ tlTest_Revision.cpp
@@ -0,0 +1,25 @@
+#include "timeline.h"
+using namespace std;
+int main()
+{
+  
+    cout << "Standard example 1" << endl;
+    Standard_Timeline t0 = Standard_Timeline(15, 25, ILLUM, 30, CONTINUOUS);
+    t0.print();
+
+    cout << "Standard example 2" << endl;
+                                          // TOT STOF  Mark MarkTOF type
+    Standard_Timeline t1 = Standard_Timeline(20, 31, WP, 40, INTERRUPTED);
+    t1.print();
+
+    cout << "Non-Standard example 3" << endl;
+    std::vector<fireRange> frVec;
+    
+    frVec.push_back(fireRange{ -2, 1 }); // 1 - (-2) = 3 min interval // 2min before TOT 1 min after
+    //cout<<"gap"<<endl;               // 3 - 1 = 2 min interval
+    frVec.push_back(fireRange{ 3, 5 }); // 5 - 3 = 2 min interval // 
+    
+    NonStandard_Timeline nST = NonStandard_Timeline(40, 30, WP, 40, frVec, 30);
+    nST.print();
+    
+}
