8470ec2c28dafb72018d80b81737a5d9c4fe3d5f
dsmith265 <72399454+dsmith265@users.noreply.github.com>
2022-01-19 09:58:17 -0700

    math should be good on non-standard
    backup before I start deleting / cleaning up the files

---
+++ 1-19-2022_BeforeCleanUp/main.cpp
@@ -0,0 +1,12 @@
+#include "mainwindow.h"
+
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.resize(1337, 1010); //set window size to allow more space for the timeline display
+    w.show();
+    return a.exec();
+}
