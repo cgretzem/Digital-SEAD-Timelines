e6a46749362b887d0e96a99453d86a0bf9ad919d
TapHereToAddText <70869886+TapHereToAddText@users.noreply.github.com>
2022-04-02 18:30:18 -0700

    Optimization of existing code
    added function for error checking which will be called from on_CalculateTimelineButton_clicked()
    added TimelineDisplayGuns array to allow easier writing to a specific timeline in on_CalculateTimelineButton_clicked() function
    other changes to increase brevity

---
+++ mainwindow.h
@@ -0,0 +1,103 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <string>
+#include <iostream>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+
+    void on_fileNameButton_clicked();
+
+    bool invalidChar2(std::string input3);
+
+    void on_fireTypeSelector_currentIndexChanged(int index);
+
+    void on_MarkingSelector_currentIndexChanged(int type);
+
+    void on_AddFiringRangeButton_clicked();
+
+    void on_AddMarkingRangeButton_clicked();
+
+    bool CalculateTimelineErrors(int timelineType, std::string *inputs);
+
+    void on_CalculateTimelineButton_clicked();
+
+    bool isInt(std::string input);
+
+    bool invalidChar(std::string input2);
+
+    void on_clearButton_clicked();
+
+    void ClearTimeline(int index);
+
+    void on_clearButtonGuns_clicked();
+
+    void on_RemoveRangeButton_clicked();
+
+    void on_RemoveRangeButton_2Mark_clicked();
+
+    //void on_CopyGunButton_clicked();
+
+    void selectGun(int index);
+
+    void on_Gun_1_button_clicked();
+
+    void on_Gun_2_button_clicked();
+
+    void on_Gun_3_button_clicked();
+
+    void on_Gun_4_button_clicked();
+
+    void on_Gun_5_button_clicked();
+
+    void on_Gun_6_button_clicked();
+
+    //void setNewData();
+
+    //void on_input_2_textEdited(const QString &arg1);
+
+    //void on_input_1_textEdited(const QString &arg1);
+
+    //void on_input_5_textEdited(const QString &arg1);
+
+    //void on_input_3_textEdited(const QString &arg1);
+
+    //void on_MarkingSelector_currentIndexChanged(int index);
+
+    void on_RemoveSingleRange_clicked();
+
+    void on_RemoveSingleMarking_clicked();
+
+    void on_ClearTimeline1_clicked();
+
+    void on_ClearTimeline2_clicked();
+
+    void on_ClearTimeline3_clicked();
+
+    void on_ClearTimeline4_clicked();
+
+    void on_ClearTimeline5_clicked();
+
+    void on_ClearTimeline6_clicked();
+
+private:
+    Ui::MainWindow *ui;
+
+    int numFiringRangesAdded;
+    int numMarkingRangesAdded;
+
+};
+#endif // MAINWINDOW_H
